2334f96ee69e9cbe3773affe7c1dd5f5ebcf478c
dldidfh <dldidfh2@gmail.com>
2022-09-22 20:53:23 +0900

    ADD : log file extension type parameterize

--- CallLog.py
+++ CallLog.py
@@ -37,11 +37,12 @@ class CallLog:
         NOTSET : logging.NOTSET,
     }
 
-    def __init__(self, log_name, log_level : Union[str, int]) -> None:
+    def __init__(self, log_name,  log_level : Union[str, int], log_extension=".log",) -> None:
         self._logger = logging.getLogger(log_name)
+        self._log_extension = self._extension_check(log_extension)
         self._file_handler = None 
         self._stream_handler = None 
-        self._root_logger_log_level = self._call_lever( log_level )
+        self._root_logger_log_level = self._call_level( log_level )
         self._logger.setLevel(self._root_logger_log_level)
 
     def __del__(self):
@@ -49,9 +50,14 @@ class CallLog:
             if handler.name == "file_handler":
                 self._logger.removeHandler(handler)
                 handler.close()
-
     @staticmethod
-    def _call_lever(level:Union[int, str]):
+    def _extension_check(extension):
+        if extension[0] == ".":
+            return extension
+        else:
+            return "."+extension
+    @staticmethod
+    def _call_level(level:Union[int, str]):
         if isinstance(level, str):
             level = CallLog._str_Level[level[0].lower()]
         elif isinstance(level, int):
@@ -62,11 +68,11 @@ class CallLog:
 
     def add_file_handler(self, path:str, H_type=None, when="midnight", interval=1, log_level:Union[str,int]=None, name="file_handler") -> None:
         if H_type == "rotating" or H_type == "R":
-            handler = TimedRotatingFileHandler(filename=f'{path}_{str(datetime.now().strftime(CallLog._TIME_FORMAT))}.log', encoding='utf-8', when=when, interval=interval)
+            handler = TimedRotatingFileHandler(filename=f'{path}_{str(datetime.now().strftime(CallLog._TIME_FORMAT))}{self._log_extension}', encoding='utf-8', when=when, interval=interval)
         else: 
-            handler = logging.FileHandler(filename=f'{path}_{str(datetime.now().strftime(CallLog._TIME_FORMAT))}.log',mode='a', encoding='utf-8')
+            handler = logging.FileHandler(filename=f'{path}_{str(datetime.now().strftime(CallLog._TIME_FORMAT))}{self._log_extension}',mode='a', encoding='utf-8')
         if log_level != None :
-            log_level = self._call_lever(log_level)
+            log_level = self._call_level(log_level)
             assert self._root_logger_log_level <= log_level, "Handler level is low than root log level"
             handler.setLevel(log_level)
         handler.setFormatter(CallLog.TimeFormatter(CallLog._FILE_HANDLER_FORMAT))
@@ -81,7 +87,7 @@ class CallLog:
         elif H_type == "stream":
             handler = logging.StreamHandler()
         if log_level != None:
-            log_level = self._call_lever(log_level)
+            log_level = self._call_level(log_level)
             assert self._root_logger_log_level <= log_level, "Handler level is low than root log level"
             handler.setLevel(log_level)
         handler.name = name
@@ -102,7 +108,11 @@ class CallLog:
         self._logger.exception(msg, *args, **kwargs)
     def critical(self,msg, *args, **kwargs):
         self._logger.critical(msg, *args, **kwargs)
-
+    def handle_exception(self, exc_type, exc_value, exc_traceback):
+        self._logger.error(
+            "Unexpected exception",
+            exc_info=(exc_type, exc_value, exc_traceback)
+        )
     class TimeFormatter(logging.Formatter):
         """logging.Formatter에 타임존 내부 설정"""
         timezone = 'Asia/Seoul'
--- example.py
+++ example.py
@@ -1,11 +1,12 @@
 from CallLog import CallLog
 
-stream_logger = CallLog(log_name="stream", log_level="debug")
-stream_logger.add_stream_handler(H_type='rich', log_level="d")
-stream_logger.add_file_handler(path="log", H_type="R", log_level="i")
+stream_logger = CallLog(log_name="stream", log_level="debug", log_extension="txt")
+stream_logger.add_stream_handler(H_type='rich', log_level="debug")
+stream_logger.add_file_handler(path="log", H_type="R", log_level="info")
 
 file_logger = CallLog(log_name="file", log_level="debug")
-file_logger.add_file_handler(path="savelog", H_type="R", log_level="d")
+file_logger.add_file_handler(path="savelog", H_type="R", log_level="degub")
 
 stream_logger.debug("qwe")
 file_logger.info("asd")
+raise
